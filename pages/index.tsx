d7d11c114c020380381ee050f661ff2c8cc2304e
MiladANaeini <milad.naeini@devoteam.com>
2022-12-07 12:10:17 +0100

    snow

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       </Head>
 
       <main id="main" className="dark">
-        {/* <LazyLoadSnowfall /> */}
+        <LazyLoadSnowfall />
 
         <div className=" lg:hidden">
           <SvgBgMobile />
