2d46bdb03aa9618505c359bfe554585025810da5
MiladANaeini <milad.naeini@devoteam.com>
2022-12-12 15:38:46 +0100

    removed unused files

--- pages/svgComponents/svgBgDesktop.tsx
+++ pages/svgComponents/svgBgDesktop.tsx
@@ -26,11 +26,6 @@ export default function SvgBgDesktop() {
   const bike1 = useRef<SVGGraphicsElement>(null);
   const bike2 = useRef<SVGGraphicsElement>(null);
   const bike3 = useRef<SVGGraphicsElement>(null);
-  const [showWiggle, setShowWiggle] = useState<boolean>(false);
-  const wiggle = () => {
-    setShowWiggle(!showWiggle);
-  };
-  setInterval(wiggle, 16000);
 
   useEffect(() => {
     setTimeout(() => {
@@ -366,11 +361,7 @@ export default function SvgBgDesktop() {
         <g className="duration-300 cursor-pointer hover:scale-y-[1.02]">
           <Cloud5 />
         </g>
-        <Moon
-          cssClass={`${
-            showWiggle ? "animate-moon" : "animation-play-state:paused"
-          }  dark:block hidden`}
-        />
+        <Moon cssClass={`animate-moon dark:block hidden`} />
         <Sun cssClass={`animate-moon dark:hidden block`} />
         <g className="animate-plane">
           <svg
